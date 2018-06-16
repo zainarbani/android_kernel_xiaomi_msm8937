b89d3e1bb7d7e171f5cc9c17d519ce9b12de2967
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2018-06-15 23:35:48 -0300

    Optmize audio codec!! -> Better audio quality; -> Louder; -> Clean; -> Safe.
    (cherry picked from commit 5484d30395545a544d1e7fe6958520e2d5898088)
    Signed-off-by: LordShenron <priyanshupandeydelhi@gmail.com>

--- sound/soc/codecs/msm8x16-wcd.h
+++ sound/soc/codecs/msm8x16-wcd.h
@@ -54,7 +54,7 @@
 #define MSM89XX_VDD_SPKDRV_NAME "cdc-vdd-spkdrv"
 
 #define DEFAULT_MULTIPLIER 800
-#define DEFAULT_GAIN 9
+#define DEFAULT_GAIN 10
 #define DEFAULT_OFFSET 100
 
 
@@ -81,14 +81,14 @@ enum codec_versions {
 
 /* Support different hph modes */
 enum {
-	NORMAL_MODE = 0,
-	HD2_MODE,
+	HD2_MODE = 0,
+	NORMAL_MODE,
 };
 
 /* Codec supports 1 compander */
 enum {
-	COMPANDER_NONE = 0,
-	COMPANDER_1, /* HPHL/R */
+	COMPANDER_1 = 0, /* HPHL/R */
+	COMPANDER_NONE,
 	COMPANDER_MAX,
 };
 
@@ -101,10 +101,10 @@ enum wcd_curr_ref {
 };
 
 enum wcd_mbhc_imp_det_pin {
-	WCD_MBHC_DET_NONE = 0,
+	WCD_MBHC_DET_BOTH = 0,
 	WCD_MBHC_DET_HPHL,
 	WCD_MBHC_DET_HPHR,
-	WCD_MBHC_DET_BOTH,
+	WCD_MBHC_DET_NONE,
 };
 
 
